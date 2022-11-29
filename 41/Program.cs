7a814e5eaf5eea79ba6269ff5282cbf8cc6a51fb
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-30 04:57:21 +0500

    Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.

---
+++ 41/Program.cs
@@ -0,0 +1,16 @@
+﻿    /*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+    0, 7, 8, -2, -2 -> 2
+    1, -7, 567, 89, 223-> 3 */
+    Console.WriteLine("Введите числа через запятую ");
+    int[] ar = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+    int count = 0;
+
+    for (int i = 0; i < ar.Length; i++)
+    {
+    if (ar[i] > 0)
+    {
+    count++;
+    }
+    }
+    Console.WriteLine($"Вы ввели чисел больше 0 -> {count}");
+

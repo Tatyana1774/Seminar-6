6c2269aec23c9d2be0910a68f3bbc294fd50d02c
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-30 05:04:04 +0500

    Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.

---
+++ 43/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+double b1 = ReadInt("Введите значение b1: ");
+double k1 = ReadInt("Введите значение k1: ");
+double b2 = ReadInt("Введите значение b2: ");
+double k2 = ReadInt("Введите значение k2: ");
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+double ReadInt(string text)
+{
+Console.WriteLine(text);
+return Convert.ToDouble(Console.ReadLine());
+}
